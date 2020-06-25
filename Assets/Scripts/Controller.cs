6e0487f15263dda5f58131766fa83e34ec1bc442
Kazuma Ohara <kzAme79@gmail.com>
2020-06-26 00:05:46 +0900

    breakout見本

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (transform.position.z < 4)
+            {
+                transform.position += transform.forward * 0.15f;
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            if (transform.position.z > -4)
+            {
+                transform.position -= transform.forward * 0.15f;
+            }
+        }
+    }
+}
