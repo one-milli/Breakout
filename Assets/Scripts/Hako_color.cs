893c31eb8bdbd1cc3c15c117f4f548db7db51298
DESKTOP-VIJKAJF\lesse <lesseraroma@outlook.jp>
2020-06-26 03:23:44 +0900

    縁をゲーミングカラー(お気持ち)にした

---
+++ Assets/Scripts/Hako_color.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hako_color : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public float H = 0, S = 0;
+
+    // Update is called once per frame
+    public bool H_ = true;
+    void Update()
+    {
+        for (int i = 1; i <= 4; i += 1)
+        {
+            GetComponent<Renderer>().material.color = UnityEngine.Color.HSVToRGB(H, 5, 100);
+            if (H_)
+            {
+                H += (float)0.0005;
+                if (H >= 1) H_ = false;
+            }
+            else
+            {
+                H -= (float)0.0005;
+                if (H <= 0) H_ = true; 
+            }
+        }
+        //S += 0.01f;
+        //if (S >= 0.1f) S = 0.0f;
+    }
+}
