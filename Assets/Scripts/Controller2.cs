4c06eeae3122205f4f71d912574032e7bf80feff
Kazuma Ohara <kzAme79@gmail.com>
2020-06-27 03:25:00 +0900

    ホッケーだいたいできた

--- Assets/Scripts/Controller2.cs
+++ Assets/Scripts/Controller2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+public class Controller2 : MonoBehaviour
 {
 
     // Use this for initialization
@@ -14,14 +14,14 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.L))
         {
             if (transform.position.z < 8)
             {
                 transform.position += transform.forward * 0.15f;
             }
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.J))
         {
             if (transform.position.z > -8)
             {
