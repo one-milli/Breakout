b063d54ccf7dbceddb728a0c40c0f96bdcc7eb42
Kazuma Ohara <kzAme79@gmail.com>
2020-06-27 02:03:20 +0900

    バグった

--- Assets/Scripts/New_ball.cs
+++ Assets/Scripts/New_ball.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class new_ball : MonoBehaviour
+public class New_ball : MonoBehaviour
 {
+    public bool LAUNCH=false;
+    public float nowTime;
+    public GameObject NewBall;
     // Start is called before the first frame update
     void Start()
     {
-        
+        nowTime = 0;
+        NewBall.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        nowTime += Time.deltaTime;
+        if(nowTime>=3) {
+            NewBall.SetActive(true);
+            transform.eulerAngles = new Vector3(0, Random.Range(30, 120), 0);
+            NewBall.GetComponent<Rigidbody>().AddForce(transform.forward * 500);
+        }
     }
 }
