e161f524a24e6658c503f4a1c7ec46ac732f0881
Kazuma Ohara <kzAme79@gmail.com>
2020-06-27 03:29:27 +0900

    発射角度変えた

--- Assets/Scripts/StartShot.cs
+++ Assets/Scripts/StartShot.cs
@@ -7,7 +7,7 @@ public class StartShot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.eulerAngles = new Vector3(0, Random.Range(30, 120), 0);
+        transform.eulerAngles = new Vector3(0, Random.Range(30, 70), 0);
         gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 500);
     }
 
