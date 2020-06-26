41c6e287acc8bbd0c7e362f832b00d9452a5cd7c
DESKTOP-VIJKAJF\lesse <lesseraroma@outlook.jp>
2020-06-27 02:22:24 +0900

    ボール二個目が時間がたつと出てくるようにした。

---
+++ Assets/Scripts/new_ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class new_ball : MonoBehaviour
+{
+    public GameObject ball2;
+    // Start is called before the first frame update
+    public static int cnt = 0;
+    void Start()
+    {
+        ball2.SetActive(false);
+        cnt = 0;
+    }
+
+    void Update()
+    {
+        if (cnt == 600)
+        {
+            ball2.SetActive(true);
+            transform.eulerAngles = new Vector3(0, Random.Range(30, 120), 0);
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 500);
+        }
+        cnt++;
+    }
+}
