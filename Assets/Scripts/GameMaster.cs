7712c362604d0ff1c897a51681cf7d71d38e3b5c
Kazuma Ohara <kzAme79@gmail.com>
2020-07-13 13:21:43 +0900

    残りブロック数を表示

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -2,27 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class GameMaster : MonoBehaviour {
+public class GameMaster : MonoBehaviour
+{
 
     public int boxNum;
     public float nowTime;
+    public Text remain;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         nowTime = 0;
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         nowTime += Time.deltaTime;
-        if (boxNum <= 0) {
-            GameOver(nowTime.ToString("F0")+"秒でクリア！");
+        if (boxNum <= 0)
+        {
+            GameOver(nowTime.ToString("F0") + "秒でクリア！");
         }
+        remain.text = boxNum.ToString("F0");
     }
 
-    public void GameOver(string resultMessage) {
-        DataSender.resultMessage=resultMessage;
+    public void GameOver(string resultMessage)
+    {
+        DataSender.resultMessage = resultMessage;
         SceneManager.LoadScene("Result");
     }
 }
